f0a5c0e6065ef69237a98a340287ebe7327ea266
Camila Rose Rodrigues da Silva <camila.silva@pampa.compasso>
2022-07-16 17:47:22 -0300

    challenge-01/challenge-01.js

---
+++ challenge-01/challenge-01.js
@@ -0,0 +1,20 @@
+const myvar = 10;
+const soma = 15 + 8;
+const souninja = true;
+const comida = ['arroz', 'feijão' , 'ovo']
+
+soma += 1;
+soma *= 3;
+
+comida[1]
+
+soma === myvar ? true : false;
+
+myvar <= soma ? true : false;
+
+function divisao(num1, num2){
+   return num1/num2;
+}
+
+
+divisao(10, 2);
